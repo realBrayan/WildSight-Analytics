307f3745760275f934f878efa200a4c2c45f704b
Umang Vadhar <umang.vadhar@gmail.com>
2024-10-25 16:11:21 -0700

    ui headers good

--- src/pages/SpeciesMap.jsx
+++ src/pages/SpeciesMap.jsx
@@ -4,7 +4,21 @@ import MiniMap from "../features/MapFeatures/MiniMap";
 function SpeciesMap() {
   return (
     <>
-      <HeaderMessage header={"Map"} description={"Track invasive species"} />
+      {/* <HeaderMessage header={"Map"} description={"Track invasive species"} /> */}
+      <div className="flex items-center justify-center mt-24 mb-[6rem]">
+        {/* <HeaderMessage
+          header={"Upload Image"}
+          description={"Report invasive species"}
+          /> */}
+        <div>
+          <h2 className="text-center text-base/7 font-semibold text-indigo-600">
+            Species Map
+          </h2>
+          <p className="mx-auto mt-2 max-w-lg text-balance text-center text-4xl font-semibold tracking-tight text-gray-950 sm:text-5xl">
+            Track invasive species
+          </p>
+        </div>
+      </div>
       <MiniMap />
     </>
   );
